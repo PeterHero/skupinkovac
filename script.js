8d1cd909ba2dd08765789ae2ab9cef536fe90cec
Petr Hrdina <pedrito.hrdina@gmail.com>
2023-09-21 23:20:39 +0200

    Functional model of groups, but yet without stats

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 class Person
 {
     name;
-    preferedPeople = {};
+    group;
+    prefs = {};
 
-    constructor(name){
+    constructor(name, group){
         this.name = name;
+        this.group = group;
     }
 }
 
-//////
-
 function addPerson(){
-    var inputName = document.getElementById('input-name');
+    const inputName = document.getElementById('input-name');
     var name = inputName.value;
     if (name == "")
         return;
@@ -30,12 +30,22 @@ function addPerson(){
     personBox.onclick = function() {showPreferences(name)};
     document.getElementById("people").appendChild(personBox);
 
-    var newPerson = new Person(name);
+    const personBoxDrag = document.createElement("div");
+    personBoxDrag.textContent = name;
+    personBoxDrag.draggable = true;
+    personBoxDrag.addEventListener('dragstart', function(event){
+        draggedElement = event.target;
+        draggedPerson = getPersonByName(draggedElement.textContent);
+    })
+    document.getElementById("unsorted").appendChild(personBoxDrag);
+
+    var newPerson = new Person(name, unsorted);
     people.push(newPerson);
+    unsorted.push(newPerson);
 }
 
 function showPreferences(name){
-    var preferencesDiv = document.getElementById('preferences');
+    const preferencesDiv = document.getElementById('preferences');
     preferencesDiv.innerHTML = '';
 
     for (let i = 0; i < people.length; i++) {
@@ -46,7 +56,7 @@ function showPreferences(name){
         personBox.textContent = people[i].name;
 
         var authorPerson = getPersonByName(name);
-        if (people[i].name in authorPerson.preferedPeople && authorPerson.preferedPeople[people[i].name]){
+        if (people[i].name in authorPerson.prefs && authorPerson.prefs[people[i].name]){
             personBox.style.color = "Chartreuse";
         }
 
@@ -58,11 +68,11 @@ function showPreferences(name){
 function changePreferences(author, preferedPerson){
     var authorPerson = getPersonByName(author);
 
-    if (preferedPerson in authorPerson.preferedPeople){
-        authorPerson.preferedPeople[preferedPerson] = !authorPerson.preferedPeople[preferedPerson];
+    if (preferedPerson in authorPerson.prefs){
+        authorPerson.prefs[preferedPerson] = !authorPerson.prefs[preferedPerson];
     }
     else{
-        authorPerson.preferedPeople[preferedPerson] = true;
+        authorPerson.prefs[preferedPerson] = true;
     }
 
     showPreferences(author);
@@ -75,4 +85,51 @@ function getPersonByName(name){
     }
 }
 
+function removePersonFromArrayByName(array, name){
+    for (let i = 0; i < array.length; i++)
+        if (array[i].name == name)
+            array.splice(i, 1);
+}
+
+function onDrop(event){
+    event.target.append(draggedElement);
+    removePersonFromArrayByName(draggedPerson.group, draggedPerson.name);
+    if (event.target.dataset.id == null){
+        draggedPerson.group = unsorted;
+    }
+    else{
+        draggedPerson.group = groups[event.target.dataset.id];
+    }
+    draggedPerson.group.push(draggedPerson);
+
+    for (let i = 0; i < numGroups; i++) {
+        console.log(groups[i]);
+    }
+    console.log(unsorted);
+}
+
 var people = [];
+
+var unsorted = [];
+var groups = [];
+
+var draggedElement;
+var draggedPerson;
+
+const numGroups = 4;
+
+for (let i = 0; i < numGroups; i++) {
+    let newGroup = [];
+    groups.push(newGroup);
+
+    const groupDiv = document.createElement("div");
+    groupDiv.classList.add("group");
+    groupDiv.dataset.id = i.toString();
+    groupDiv.addEventListener('dragover', function(event){
+        event.preventDefault();
+    })
+    groupDiv.addEventListener('drop', function(event) {
+        onDrop(event);
+    })
+    document.getElementById("groups").appendChild(groupDiv);
+}
